ac621f2a29f927975b789aa3569622af82556397
James <z2yuan@ucsd.edu>
2020-02-04 10:19:07 -0800

    update the step count

--- app/src/main/java/com/example/wwrapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/wwrapp/HomeScreenActivity.java
@@ -1,24 +1,46 @@
 package com.example.wwrapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.wwrapp.fitness.FitnessService;
+import com.example.wwrapp.fitness.FitnessServiceFactory;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class HomeScreenActivity extends AppCompatActivity {
+    public static final String FITNESS_SERVICE_KEY = "FITNESS_SERVICE_KEY";
 
+    private static final String TAG = "StepCountActivity";
+    private TextView textSteps;
+    private FitnessService fitnessService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        textSteps = findViewById(R.id.homeStepsTextView);
+
+        String fitnessServiceKey = getIntent().getStringExtra(FITNESS_SERVICE_KEY);
+        fitnessService = FitnessServiceFactory.create(fitnessServiceKey, this);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        fitnessService.updateStepCount();
+
+        fitnessService.setup();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +57,19 @@ public class HomeScreenActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//       If authentication was required during google fit setup, this will be called after the user authenticates
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == fitnessService.getRequestCode()) {
+                fitnessService.updateStepCount();
+            }
+        } else {
+            Log.e(TAG, "ERROR, google fit result code: " + resultCode);
+        }
+    }
     //implement after getting real data
     private boolean checkHasHeight(){
         //if data exist
@@ -44,5 +79,8 @@ public class HomeScreenActivity extends AppCompatActivity {
         return hasHeight;
     }
 
+    public void setStepCount(long totalStep) {
+        textSteps.setText(String.valueOf(totalStep));
+    }
 }
 
