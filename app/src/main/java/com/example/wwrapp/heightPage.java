d666434aa46a3da9d1d3961d5d0541f5266bc31e
jinghongluo <jil040@ucsd.edu>
2020-02-04 13:08:14 -0800

    heightPage UI

--- app/src/main/java/com/example/wwrapp/heightPage.java
+++ app/src/main/java/com/example/wwrapp/heightPage.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -18,6 +19,8 @@ public class heightPage extends AppCompatActivity {
     private Spinner inchSpinner;
     private static final String[] feet = {"", "0", "1", "2", "3", "4", "5", "6", "7"};
     private static final String[] inch = {"", "0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
+    private int userFeet;
+    private int userInch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class heightPage extends AppCompatActivity {
 
         feetSpinner = (Spinner) findViewById(R.id.spinner_feet);
 
-        ArrayAdapter<String> feetAdapter = new ArrayAdapter<String>(heightPage.this,
+        final ArrayAdapter<String> feetAdapter = new ArrayAdapter<String>(heightPage.this,
                 android.R.layout.simple_spinner_item, feet);
 
         inchSpinner = (Spinner) findViewById(R.id.spinner_inch);
@@ -41,6 +44,44 @@ public class heightPage extends AppCompatActivity {
 
         inchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         inchSpinner.setAdapter(inchAdapter);
+
+        inchSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0 || position == 1){
+                    userInch = 0;
+                } else {
+                    userInch = Integer.valueOf(parent.getItemAtPosition(position).toString());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                userInch = 0;
+            }
+        });
+
+        feetSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0 || position == 1){
+                    if(userInch == 0){
+                        // show a pop up alert asking user to enter a non-zero feet.
+                    } else {
+                        userFeet = 0;
+                    }
+                } else {
+                    userFeet = Integer.valueOf(parent.getItemAtPosition(position).toString());
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                if(userInch == 0){
+                    // show a pop up alert asking user to enter a proper feet that is > 0.
+                }
+            }
+        });
 
     }
 
