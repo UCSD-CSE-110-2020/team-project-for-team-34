38803dc0db0c85e8d575310752742c70cb1512c5
Tony <tsshen@ucsd.edu>
2020-02-08 11:24:15 -0800

    Add listView for route screen

--- app/src/main/java/com/example/wwrapp/RoutesActivity.java
+++ app/src/main/java/com/example/wwrapp/RoutesActivity.java
@@ -1,14 +1,43 @@
 package com.example.wwrapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class RoutesActivity extends AppCompatActivity {
+
+    private static final String TAG = "RoutesActivity";
+
+    private ArrayList<String> mRouteName = new ArrayList<>();
+    private ArrayList<String> mRouteDate = new ArrayList<>();
+    private ArrayList<String> mRouteMile = new ArrayList<>();
+    private ArrayList<String> mRouteStep = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_routes);
+        Log.d(TAG, "onCreate: started");
+
+        loadRoutes();
+    }
+
+    private void loadRoutes(){
+        Log.d(TAG, "loadRoutes: loading routes");
+        //read Route into the arraylist
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview");
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_route);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mRouteName,mRouteDate,mRouteMile,mRouteStep,this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
