518310ccaf349fba15d71ca5387432c8d58c122b
Tony <tsshen@ucsd.edu>
2020-03-10 14:46:52 -0700

    new branch

---
+++ app/src/main/java/com/example/wwrapp/ProposedWalkScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.wwrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProposedWalkScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proposed_walk_screen);
+    }
+}
