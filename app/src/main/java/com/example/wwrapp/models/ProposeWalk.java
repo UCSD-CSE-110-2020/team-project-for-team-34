a42a73f4283ef54d36d7f2731d80f40fab74788d
Byron White <byron4white@gmail.com>
2020-03-10 14:54:25 -0700

    commit changes for pull

---
+++ app/src/main/java/com/example/wwrapp/models/ProposeWalk.java
@@ -0,0 +1,17 @@
+package com.example.wwrapp.models;
+
+import java.util.List;
+
+public class ProposeWalk {
+
+    private Route route;
+    private List<String> users;
+    private String date;
+
+    public ProposeWalk() {}
+
+    public ProposeWalk(Route route) {
+        this.route = route;
+    }
+
+}
