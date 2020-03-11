28203e41df6f49cc96b20f8932a7fe2789653f48
Tony <tsshen@ucsd.edu>
2020-03-11 16:55:12 -0700

    add proposed walk screen

---
+++ app/src/main/java/com/example/wwrapp/activities/ProposedWalkActivity.java
@@ -0,0 +1,16 @@
+package com.example.wwrapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wwrapp.R;
+
+public class ProposedWalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proposed_walk);
+    }
+}
